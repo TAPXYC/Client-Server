b8574469f26af5e54a8a1022580fcdce06c06f7e
TAPXYC <tapxyc@yandex.ru>
2021-06-09 14:18:25 +0500

    Base async logic

---
+++ AsyncServer/Comunication/WebModules/Helpers/AsyncSocketAction.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+class AsyncSocketAction
+{
+
+
+
+    #region Send region
+
+    /// <summary>
+    /// Информация об отправке сообщения
+    /// </summary>
+    struct SendState
+    {
+        /// <summary>
+        /// Сигнальное событие потоку извне
+        /// </summary>
+        public ManualResetEvent ResetEvent;
+        /// <summary>
+        /// Сокет получателя (удаленный)
+        /// </summary>
+        public Socket WorkSocket;
+        /// <summary>
+        /// Нужно ли закрывать поток
+        /// </summary>
+        public bool NeedCloseHandler;
+
+        public SendState(Socket socket, ManualResetEvent resetEvent, bool needCloseHandler)
+        {
+            WorkSocket = socket;
+            ResetEvent = resetEvent;
+            NeedCloseHandler = needCloseHandler;
+        }
+    }
+
+
+
+
+    /// <summary>
+    /// Отправка данных 
+    /// </summary>
+    /// <param name="remoteSocket">Удаленный сокет</param>
+    /// <param name="data">Данные</param>
+    /// <param name="resetEvent">Сигнальное событие потоку извне</param>
+    /// <param name="needCloseHandler">Нужно ли закрывать поток</param>
+    public static void Send(Socket remoteSocket, String data, ManualResetEvent resetEvent = null, bool needCloseHandler = false)
+    {
+        // Конвертируем сообщение в байтовый вид 
+        byte[] byteData = Encoding.UTF8.GetBytes(data);
+
+        SendState sendState = new SendState(remoteSocket, resetEvent, needCloseHandler);
+
+        // Начинаем отправлять данные клиенту  
+        remoteSocket.BeginSend(byteData, 0, byteData.Length, 0, new AsyncCallback(SendCallback), sendState);
+    }
+
+
+
+
+
+
+
+
+    /// <summary>
+    /// Асинхронная отправка сообщения
+    /// </summary>
+    /// <param name="ar">Состояние асинхронной операции</param>
+    private static void SendCallback(IAsyncResult ar)
+    {
+        try
+        {
+            SendState sendState = (SendState)ar.AsyncState;
+
+            // Получаем сокет от состояния аснхронной операции
+            Socket handler = sendState.WorkSocket;
+
+            // Завершаем отправку данных на удаленого клиента 
+            int bytesSent = handler.EndSend(ar);
+
+            if(sendState.ResetEvent != null)
+                sendState.ResetEvent.Set();
+
+            //Закрываем поток и очищаем его
+            if (sendState.NeedCloseHandler)
+            {
+                handler.Shutdown(SocketShutdown.Both);
+                handler.Close();
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+    }
+
+    #endregion
+}
+
---
+++ AsyncServer/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+
+
+
+class Programm
+{
+    public static void Main()
+    {
+        int ServerPort = 11000;
+        string otherMachineName = "DESKTOP-Q67Q8PO";
+
+        Communication communication = new Communication(ServerPort);
+        communication.CreateServer();
+
+        communication.OnClientNameNotFound += i => Console.WriteLine($"[{i}]\tне в сети");
+        communication.OnClientTimeout += i => Console.WriteLine($"[{i.RemoteIP}]\tтаймаут"); 
+
+        communication.OnServerRecieveMessage += Print;
+        communication.OnStopServer += e => Console.WriteLine("Stop server  " + e != null ? e.Message : "");
+
+        while (true)
+        {
+            string message = Console.ReadLine();
+            communication.SendMessage(message, otherMachineName);
+        }
+    }
+
+
+
+
+    static void Print(RemotePointInfo remotePoint, string message)
+    {
+        Console.WriteLine($"[{remotePoint.RemoteIP}]\t" + message);
+    }
+}
